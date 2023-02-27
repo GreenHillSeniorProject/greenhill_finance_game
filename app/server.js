369440c426bc8b49eeabff2e07c7afa40c104b4d
Andrew Maxwell <arm467@drexel.edu>
2023-02-27 18:58:31 +0000

    First successful connect

--- app/server.js
+++ app/server.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 const config = require("../config.json");
 
-const database = mysql.createConnection({
+const db = mysql.createConnection({
     host: "database-1.cpjwwrmrh8mc.us-east-1.rds.amazonaws.com",
     port: "3306",
     user: config.user,
     password: config.password,
-    databse: "test_1"
+    databse: ""
 });
+
+db.connect((err)=>{
+    if(err){
+        console.log("Ran into an error");
+    }
+    console.log("successful connection");
+})
