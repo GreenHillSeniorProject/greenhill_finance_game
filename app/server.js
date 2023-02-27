78a27d3720a3f7385f1e93f72b7a5375d1f1b421
Andrew Maxwell <arm467@drexel.edu>
2023-02-27 18:37:51 +0000

    .gitignore and removed node_modules

--- app/server.js
+++ app/server.js
@@ -1,1 +1,10 @@
+const mysql = require("mysql");
+const config = require("../config.json");
 
+const database = mysql.createConnection({
+    host: "database-1.cpjwwrmrh8mc.us-east-1.rds.amazonaws.com",
+    port: "3306",
+    user: config.user,
+    password: config.password,
+    databse: "test_1"
+});
