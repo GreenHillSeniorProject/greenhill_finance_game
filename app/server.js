2d32dbb6bd84a82957a0bc77bf59cc87111d4612
Jay Patel <jaypatel815@gmail.com>
2023-04-24 22:42:49 -0400

    pulling stock data from polygon

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const axios = require('axios');
+const config = require('../config.json');
 
 const app = express();
 
@@ -15,6 +17,37 @@ const db = mysql.createConnection({
     insecureAuth: true
 });
 
+// make a request to the polygon API to get stock info
+const getStockInfo = async (symbol) => {
+    try {
+      const response = await axios.get(`https://api.polygon.io/v1/meta/symbols/${symbol}/company?apiKey=${config.polygonApiKey}`);
+      const data = response.data;
+      return {
+        symbol: symbol,
+        price: data.lastQuotePrice,
+        description: data.description
+      };
+    } catch (error) {
+      console.error(`Error fetching stock info for symbol ${symbol}: ${error.message}`);
+      return null;
+    }
+};
+
+// insert a stock record into the database
+const insertStock = async (stock) => {
+    const sql = 'INSERT INTO Stocks (ticker, description) VALUES (?, ?)';
+    const values = [stock.symbol, stock.description];
+    return new Promise((resolve, reject) => {
+        db.query(sql, values, (error, results, fields) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+};
+
 app.post("/signup", (req, res) => {
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
@@ -22,7 +55,9 @@ app.post("/signup", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    db.query('INSERT INTO GreenhillEmployee (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)', [first_name, last_name, email, username, password], (err, result) => {
+    db.query('INSERT INTO GreenhillEmployee (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)', 
+    [first_name, last_name, email, username, password], 
+    (err, result) => {
         if (err) {
             console.log(err);
         } else {
@@ -50,6 +85,20 @@ app.post("/signin", (req, res) => {
     })
 })
 
+// main function that fetches stock info and inserts it into the database
+const main = async () => {
+    const symbols = ['AAPL', 'GOOG', 'AMZN']; // add more symbols here
+    for (const symbol of symbols) {
+      const stock = await getStockInfo(symbol);
+      if (stock !== null) {
+        await insertStock(stock);
+        console.log(`Inserted stock info for ${symbol} into database.`);
+      }
+    }
+};
+
 app.listen(3001, () => {
     console.log("local host server running")
 });
+
+main();
