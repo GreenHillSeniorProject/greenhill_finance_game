c2d73b6f49158d9fbf9a9a60017a47a24e39b57e
Jay Patel <jaypatel815@gmail.com>
2023-05-01 17:13:32 -0400

    updated main function to check for duplicates

--- app/server.js
+++ app/server.js
@@ -1,104 +1,133 @@
+// Import necessary modules
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const axios = require('axios');
 const config = require('../config.json');
 
+// Create Express app and set up middleware
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
+// Create MySQL database connection
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "password",
-    database: "GreenHill_LocalHost",
-    insecureAuth: true
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "GreenHill_LocalHost",
+  insecureAuth: true
 });
 
-// make a request to the polygon API to get stock info
+// Function to fetch stock info for a given symbol from an external API (Polygon)
 const getStockInfo = async (symbol) => {
-    try {
-      const response = await axios.get(`https://api.polygon.io/v1/meta/symbols/${symbol}/company?apiKey=${config.polygonApiKey}`);
-      const data = response.data;
-      return {
-        symbol: symbol,
-        price: data.lastQuotePrice,
-        description: data.description
-      };
-    } catch (error) {
-      console.error(`Error fetching stock info for symbol ${symbol}: ${error.message}`);
-      return null;
-    }
+  try {
+    const response = await axios.get(`https://api.polygon.io/v1/meta/symbols/${symbol}/company?apiKey=${config.polygonApiKey}`);
+    const data = response.data;
+    return {
+      symbol: symbol,
+      price: data.lastQuotePrice,
+      description: data.description
+    };
+  } catch (error) {
+    console.error(`Error fetching stock info for symbol ${symbol}: ${error.message}`);
+    return null;
+  }
 };
 
-// insert a stock record into the database
+// Function to insert stock info into MySQL database
 const insertStock = async (stock) => {
-    const sql = 'INSERT INTO Stocks (ticker, description) VALUES (?, ?)';
-    const values = [stock.symbol, stock.description];
-    return new Promise((resolve, reject) => {
-        db.query(sql, values, (error, results, fields) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      });
-    });
+  const sql = 'INSERT INTO Stocks (ticker, description) VALUES (?, ?)';
+  const values = [stock.symbol, stock.description];
+  try {
+    const result = await db.query(sql, values);
+    console.log(`Inserted stock info for ${stock.symbol} into database.`);
+    return result;
+  } catch (error) {
+    if (error.code === 'ER_DUP_ENTRY') {
+      console.log(`Skipping duplicate entry for ${stock.symbol}`);
+      return null;
+    } else {
+      throw error;
+    }
+  }
 };
 
-app.post("/signup", (req, res) => {
-    const first_name = req.body.first_name;
-    const last_name = req.body.last_name;
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
+// Function to fetch stock info for a given symbol from MySQL database
+const getStockFromDB = async (symbol) => {
+  const sql = 'SELECT * FROM Stocks WHERE ticker = ?';
+  const values = [symbol];
+  return new Promise((resolve, reject) => {
+    db.query(sql, values, (error, results, fields) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
 
-    db.query('INSERT INTO GreenhillEmployee (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)', 
-    [first_name, last_name, email, username, password], 
+// Route for handling user sign up requests
+app.post("/signup", (req, res) => {
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query('INSERT INTO GreenhillEmployee (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)',
+    [first_name, last_name, email, username, password],
     (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send({ message: 'Account created successfully' });
-        }
+      if (err) {
+        console.log(err);
+      } else {
+        res.send({ message: 'Account created successfully' });
+      }
     })
 });
 
+// Route for handling user sign in requests
 app.post("/signin", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    db.query('SELECT * FROM GreenhillEmployee WHERE email = ? AND password = ?', 
-    [email, password], 
+  db.query('SELECT * FROM GreenhillEmployee WHERE email = ? AND password = ?',
+    [email, password],
     (err, result) => {
-        if (err) {
-            res.send({ err: err });
-        } 
-        
-        if (result.length > 0) {
-            res.send(result);
-        } else {
-            res.send({message: "Account does not exist"});
-        }
+      if (err) {
+        res.send({ err: err });
+      }
+
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ message: "Account does not exist" });
+      }
     })
 })
 
-// main function that fetches stock info and inserts it into the database
+// Main function to fetch stock info for multiple symbols and insert into database
 const main = async () => {
-    const symbols = ['AAPL', 'GOOG', 'AMZN']; // add more symbols here
-    for (const symbol of symbols) {
-      const stock = await getStockInfo(symbol);
-      if (stock !== null) {
+  const symbols = ['AAPL', 'GOOG', 'AMZN']; // add more symbols here
+  for (const symbol of symbols) {
+    const stock = await getStockInfo(symbol);
+    const stockInDB = await getStockFromDB(symbol);
+    if (stock !== null && (stockInDB === null || stockInDB.length === 0)) {
+      try {
         await insertStock(stock);
         console.log(`Inserted stock info for ${symbol} into database.`);
+      } catch (error) {
+        console.error(`Error inserting stock info for ${symbol}: ${error.message}`);
       }
+    } else {
+      console.log(`Skipping duplicate entry for ${stock.symbol}`);
     }
+  }
 };
 
 app.listen(3001, () => {
-    console.log("local host server running")
+  console.log("local host server running")
 });
 
 main();
