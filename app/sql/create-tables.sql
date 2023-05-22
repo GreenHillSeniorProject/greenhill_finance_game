958ab4d441098b65d017e5a09618ecc6af403215
Jay Patel <jaypatel815@gmail.com>
2023-05-22 13:03:54 -0400

    updated tables

--- app/sql/create-tables.sql
+++ app/sql/create-tables.sql
@@ -8,43 +8,28 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema greenhill_localhost
 -- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `greenhill_localhost` DEFAULT CHARACTER SET utf8 ;
+USE `greenhill_localhost` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Admin`
+-- Table `greenhill_localhost`.`Employees`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Admin (
-  `admin_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(320) NOT NULL,
-  `last_update` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  PRIMARY KEY (`admin_id`),
-  UNIQUE INDEX `admin_id_UNIQUE` (`admin_id` ASC) VISIBLE
-  )
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `mydb`.`GreenhillEmployee`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`GreenhillEmployee` (
+CREATE TABLE IF NOT EXISTS Employees (
   `employee_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NULL,
   `email` VARCHAR(320) NOT NULL,
+  `user_type` ENUM('admin', 'employee', 'advisor') NOT NULL,
   `username` VARCHAR(25) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `picture` BLOB NULL,
   `active` TINYINT(1) NULL,
   `last_update` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `date_created` DATE NOT NULL DEFAULT (CURRENT_DATE),
+  `is_financial_advisor` TINYINT(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`employee_id`),
   UNIQUE INDEX `employee_id_UNIQUE` (`employee_id` ASC) VISIBLE,
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
@@ -52,30 +37,10 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`FinancialAdvisors`
+-- Table `greenhill_localhost`.`Stocks`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`FinancialAdvisors` (
-  `advisor_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `last_name` VARCHAR(45) NULL,
-  `email` VARCHAR(320) NOT NULL,
-  `username` VARCHAR(25) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `picture` BLOB NULL,
-  `active` TINYINT(1) NULL,
-  `last_update` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  `date_created` DATE NOT NULL DEFAULT (CURRENT_DATE),
-  PRIMARY KEY (`advisor_id`),
-  UNIQUE INDEX `advisor_id_UNIQUE` (`advisor_id` ASC) VISIBLE,
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `mydb`.`Stocks`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Stocks` (
+CREATE TABLE IF NOT EXISTS `Stocks` (
   `stock_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `cusip` VARCHAR(45) NOT NULL,
   `ticker` VARCHAR(5) NOT NULL,
   `description` TEXT NULL,
   `dividend` DECIMAL(5,2) NULL,
@@ -88,9 +53,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`GameInfo`
+-- Table `greenhill_localhost`.`GameInfo`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`GameInfo` (
+CREATE TABLE IF NOT EXISTS `GameInfo` (
   `game_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `employee_id` INT UNSIGNED NULL,
   `starting_cash` DECIMAL(5,2) NULL,
@@ -99,108 +64,73 @@ CREATE TABLE IF NOT EXISTS `mydb`.`GameInfo` (
   `last_update` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   PRIMARY KEY (`game_id`),
   UNIQUE INDEX `game_id_UNIQUE` (`game_id` ASC) VISIBLE,
-  UNIQUE INDEX `employee_id_UNIQUE` (`employee_id` ASC) VISIBLE,
   CONSTRAINT `employee_id_gameinfo`
     FOREIGN KEY (`employee_id`)
-    REFERENCES `mydb`.`GreenhillEmployee` (`employee_id`)
+    REFERENCES `greenhill_localhost`.`Employees` (`employee_id`)
     ON DELETE SET NULL
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Portfolios`
+-- Table `greenhill_localhost`.`Portfolios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Portfolios` (
+CREATE TABLE IF NOT EXISTS `Portfolios` (
   `portfolio_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `portfolio_name` VARCHAR(45) NOT NULL,
-  `advisor_id` INT UNSIGNED NOT NULL,
+  `employee_id` INT UNSIGNED NOT NULL,
   `game_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`portfolio_id`),
-  UNIQUE INDEX `portfolio_id_UNIQUE` (`portfolio_id` ASC) VISIBLE,
-  UNIQUE INDEX `advisor_id_UNIQUE` (`advisor_id` ASC) VISIBLE,
-  UNIQUE INDEX `game_id_UNIQUE` (`game_id` ASC) VISIBLE,
-  CONSTRAINT `advisor_id_portfolios`
-    FOREIGN KEY (`advisor_id`)
-    REFERENCES `mydb`.`FinancialAdvisors` (`advisor_id`)
+  CONSTRAINT `employee_id_portfolios`
+    FOREIGN KEY (`employee_id`)
+    REFERENCES `greenhill_localhost`.`Employees` (`employee_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `game_id_portfolios`
     FOREIGN KEY (`game_id`)
-    REFERENCES `mydb`.`GameInfo` (`game_id`)
+    REFERENCES `greenhill_localhost`.`GameInfo` (`game_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`GameAdvisors`
+-- Table `greenhill_localhost`.`PortfolioStock`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`GameAdvisors` (
-  `game_id` INT UNSIGNED NOT NULL,
-  `advisor_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`game_id`, `advisor_id`),
-  INDEX `advisor_id_idx` (`advisor_id` ASC) VISIBLE,
-  CONSTRAINT `advisor_id_gameadvisors`
-    FOREIGN KEY (`advisor_id`)
-    REFERENCES `mydb`.`FinancialAdvisors` (`advisor_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `game_id_gameadvisors`
-    FOREIGN KEY (`game_id`)
-    REFERENCES `mydb`.`GameInfo` (`game_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`PortfolioStock`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PortfolioStock` (
+CREATE TABLE IF NOT EXISTS `PortfolioStock` (
   `portfolio_id` INT UNSIGNED NOT NULL,
   `stock_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`portfolio_id`, `stock_id`),
   INDEX `stock_id_idx` (`stock_id` ASC) VISIBLE,
-  UNIQUE INDEX `portfolio_id_UNIQUE` (`portfolio_id` ASC) VISIBLE,
-  UNIQUE INDEX `stock_id_UNIQUE` (`stock_id` ASC) VISIBLE,
   CONSTRAINT `portfolio_id`
     FOREIGN KEY (`portfolio_id`)
-    REFERENCES `mydb`.`Portfolios` (`portfolio_id`)
+    REFERENCES `greenhill_localhost`.`Portfolios` (`portfolio_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `stock_id_portfoliostock`
     FOREIGN KEY (`stock_id`)
-    REFERENCES `mydb`.`Stocks` (`stock_id`)
+    REFERENCES `greenhill_localhost`.`Stocks` (`stock_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `mydb`.`StockHistory`
+-- Table `greenhill_localhost`.`StockHistory`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`StockHistory` (
+CREATE TABLE IF NOT EXISTS `StockHistory` (
   `record_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `stock_id` INT UNSIGNED NOT NULL,
   `price` DECIMAL(5,2) NULL,
   `cusip` VARCHAR(10) NOT NULL,
-  `ticker` VARCHAR(5) NOT NULL,
-  `description` VARCHAR(255) NULL,
-  `dividend` DECIMAL(5,2) NULL,
-  `payment_freq` INT NULL,
   `timestamp` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  primary key (`record_id`),
-  UNIQUE INDEX `ticker_UNIQUE` (`ticker` ASC) VISIBLE,
-  UNIQUE INDEX `stock_id_UNIQUE` (`stock_id` ASC) VISIBLE,
+  PRIMARY KEY (`record_id`),
+  UNIQUE INDEX `stock_id_timestamp_UNIQUE` (`stock_id`, `timestamp`) VISIBLE,
   CONSTRAINT `stock_id_stockhistory`
     FOREIGN KEY (`stock_id`)
-    REFERENCES `mydb`.`Stocks` (`stock_id`)
+    REFERENCES `greenhill_localhost`.`Stocks` (`stock_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
-
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
