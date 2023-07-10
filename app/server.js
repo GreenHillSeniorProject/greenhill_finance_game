47ff9050d38e94e96062e909a2a0c4ff88e6a131
Clev Cong <cccongx@gmail.com>
2023-07-10 16:52:51 -0400

    added new create-tables sql file

--- app/server.js
+++ app/server.js
@@ -45,7 +45,7 @@ app.get('/faq', (req, res) => {
       res.json(data);
       console.log(data);
     };
-});
+  });
 });
 
 // Function to fetch stock info for a given symbol from an external API (Polygon)
@@ -68,32 +68,6 @@ const delay = 5 * 60 * 1000; // 5 minutes
 
 // Function to delay the execution for the specified duration
 const sleep = (duration) => new Promise((resolve) => setTimeout(resolve, duration));
-
-// Function to fetch stock info for a given symbol from an external API (Yahoo)
-/* const getStockInfo = async (symbol) => {
-  const options = {
-    method: 'GET',
-    url: `https://yahoo-finance127.p.rapidapi.com/price/${symbol}`,
-    headers: {
-      'X-RapidAPI-Key': config.yahooApiKey,
-      'X-RapidAPI-Host': 'yahoo-finance127.p.rapidapi.com'
-    }
-  };
-
-  try {
-    const response = await axios.request(options);
-    const data = response.data;
-    return {
-      symbol: symbol,
-      price: data.regularMarketPrice,
-      description: data.longName
-    };
-  } catch (error) {
-    console.error(`Error fetching stock info for symbol ${symbol}: ${error.message}`);
-    return null;
-  }
-};
- */
 
 // Function to insert stock info into MySQL database
 const insertStock = async (stock) => {
@@ -184,7 +158,7 @@ const validateSave = async (portfolioId) => {
   }
 
   // check num of unique stocks
-  const numStocks = await(fetchStockCount);
+  const numStocks = await(fetchStockCount(portfolioId));
   
   // check non negative cash balance
 }
@@ -672,36 +646,6 @@ const main = async () => {
     });
     
 };
-
-
-// Main function to fetch stock info for multiple symbols and insert into database using Yahoo Finance API
-/* const main = async () => {
-  const symbols = [];
-  fs.createReadStream('constituents.csv')
-    .pipe(csv())
-    .on('data', (data) => {
-      symbols.push(data.Symbol);
-    })
-    .on('end', async () => {
-      for (const symbol of symbols) {
-        const stock = await getStockInfo(symbol);
-        const stockInDB = await getStockFromDB(symbol);
-        if (stock !== null && (stockInDB === null || stockInDB.length === 0)) {
-          try {
-            await insertStock(stock);
-            console.log(`Inserted stock info for ${symbol} into the database.`);
-          } catch (error) {
-            console.error(`Error inserting stock info for ${symbol}: ${error.message}`);
-          }
-        } else {
-          console.log(`Skipping duplicate entry for ${symbol}`);
-        }
-        // Delay between requests to avoid rate limiting
-        await sleep(delay);
-      }
-    });
-}; */
-
 
 /*
 app.post("/create", (req, res) => {
