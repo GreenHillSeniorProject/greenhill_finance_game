efad3c7b182b1d560bbac29b857f22e110d26e9e
Andrew8ter <arm467@drexel.edu>
2023-08-17 15:46:59 -0400

    small fixes for consistent syntax

--- app/controllers/dbController.js
+++ app/controllers/dbController.js
@@ -1,12 +1,10 @@
 let mysql = require("mysql2");
 let config = require('../../config.json');
 
-const db = mysql.createConnection({
+exports.db = mysql.createConnection({
 user: "root",
 host: "localhost",
 password: config.localhost_password,
 database: config.db_name,
 insecureAuth: true
 });
-
-module.exports = db;
