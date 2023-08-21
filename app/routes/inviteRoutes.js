75112944f1638acea813fee17db0c6fd694ef3dc
Andrew8ter <arm467@drexel.edu>
2023-08-21 19:06:26 -0400

    db queries fixed

--- app/routes/inviteRoutes.js
+++ app/routes/inviteRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 let util = require('util');
-let db = require('../controllers/dbController');
+let dbCTLR = require('../controllers/dbController');
+let db = dbCTLR.db;
 let jwtCTLR = require('../controllers/tokenController');
 
 //bracket notation [] only used because of the dash in the route
 let invite_mailto = async (req, res) => {
 
-  console.log(req.headers);
   const authHeader = req.headers.authorization;
-  console.log("token", authHeader);
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'Invalid authorization header' });
@@ -21,13 +20,18 @@ let invite_mailto = async (req, res) => {
   console.log("email:", email);
   
   let mailto = await createInviteEmail(first_name, last_name, email, inviterToken);
+  console.log("successful mailto created");
   res.send(mailto);
 };
 
 //shouldn't need the user_id once tokens become available
 const createInviteEmail = async (first_name, last_name, email, token) => {
   let code = generateReferralCode();
-  let user_id = jwtCTLR.getIdFromToken(token);
+  let user_id = jwtCTLR.getIdFromToken(token).userId;
+  console.log("user_id: ", user_id);
+  console.log("email: ", email);
+  console.log("first name: ", first_name);
+  console.log("last name: ", last_name);
   var subject = "Invitation to Field Goal Finance";
   var body = "Hello " + first_name + " " + last_name + "!\n\nDo you have what it takes to outperform your peers? You have been cordially \
 invited to a unique and exclusive gaming community!\n\n\
@@ -45,6 +49,7 @@ Thank you for playing!";
 
   //insert the invite to the Referrals table in the database
   //NOTE: The referrer ID will be dummy data for now, delete once auth tokens are set up
+  console.log("made it to query")
   const referralInsertQuery = 'INSERT INTO Referrals (referrer_id, referred_email, referral_code, status, expiration_date) VALUES (?, ?, ?, ?, ?)';
   const expiration_date = new Date();
   expiration_date.setDate(expiration_date.getDate() + 7); // Set the expiration date to 7 days from the current date
