a41cc97b23ed04c396276a1893f9a8073dfd8796
Andrew8ter <arm467@drexel.edu>
2023-08-28 17:42:30 -0400

    debugging homepage code

--- app/routes/homepageRoutes.js
+++ app/routes/homepageRoutes.js
@@ -17,14 +17,34 @@ let homepage_main = async (req, res, next) => {
 		try {
 			const userId = await authMiddleware.getUserIdFromToken(token);
 		
-			// Fetch user data from the database based on userId
-			const user = await db.getUserById(userId); // Implement the function to retrieve user data
-			const currGameUsers = await db.fetchCurrentGameUsers(userId);
-			const pastGames = await db.fetchPastGames(userId);
-			const aveRank = await db.fetchAveRanking(userId);
-			const firstRank = await db.fetchNumber1stRankedGames(userId);
-			const secondRank = await db.fetchNumber2ndRankedGames(userId);
-			const thirdRank = await db.fetchNumber3rdRankedGames(userId);
+			try {
+				const user = await db.getUserById(userId);
+				if (!user) throw new Error("User data not found");
+			  
+				const currGameUsers = await db.fetchCurrentGameUsers(userId);
+				if (!currGameUsers) throw new Error("Current game users not found");
+			  
+				const pastGames = await db.fetchPastGames(userId);
+				if (!pastGames) throw new Error("Past games not found");
+			  
+				const aveRank = await db.fetchAveRanking(userId);
+				if (!aveRank) throw new Error("Average ranking not found");
+			  
+				const firstRank = await db.fetchNumber1stRankedGames(userId);
+				if (!firstRank) throw new Error("First ranked games not found");
+			  
+				const secondRank = await db.fetchNumber2ndRankedGames(userId);
+				if (!secondRank) throw new Error("Second ranked games not found");
+			  
+				const thirdRank = await db.fetchNumber3rdRankedGames(userId);
+				if (!thirdRank) throw new Error("Third ranked games not found");
+			  
+			  } catch (error) {
+				console.log("Error: ", error.message);
+				const err = new Error(`Failed at operation: ${error.message}`);
+				err.status = 400;
+				next(err);
+			  }
 		
 			// Construct and send the response
 			const responseData = {
